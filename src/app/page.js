76aacd02378c8011dd20ca1a474e3fedfd9f52db
Michaelson44 <149709538+Michaelson44@users.noreply.github.com>
2025-02-22 05:39:03 -0800

    nav update

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,13 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
 
+  
+
   const router = useRouter();
+
+  const handleRoute = (link) => {
+    return router.push(link)
+  }
   return (
     <>
       <div style={{display: "flex", flexDirection: "column", gap: "15px", alignItems: "center"}}>
@@ -15,9 +21,9 @@ export default function Home() {
       </div>
 
       <div style={{display: "flex", flexDirection: "column-reverse", marginTop: "100px", gap: "10px"}}>
-        <button onClick={()=> router.push("/about-us")}>about</button>
-        <button onClick={() => router.push("/contact-us")}>contact</button>
-        <button onClick={() => router.push("/FAQ")}>faq</button>
+        <button onClick={()=> handleRoute("/about-us")}>about</button>
+        <button onClick={() => handleRoute("/contact-us")}>contact</button>
+        <button onClick={() => handleRoute("/FAQ")}>faq</button>
       </div>
     </>
   );
